cd59ceeb600146bd99bbfd4e4dca792faad97110
gjermundnor <gjermundnor@outlook.com>
2016-10-29 16:32:56 +0200

    animate when user scrolls by

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import GSAP from 'gsap';
+import ScrollMagic from 'scrollmagic';
 import Loop from './loop';
-
-require('./portfolio.scss');
+//import 'script!scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js';
+import 'imports?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
 
 const portfolios = [
   {
@@ -37,18 +38,25 @@ export default class Portfolio extends React.Component {
   }
   animateSingles(){
     const portfolioImage = document.getElementsByClassName('portfolio-image');
-    TweenMax.set(portfolioImage, {scale: 0.6});
-    TweenMax.staggerTo(portfolioImage, 0.8, {opacity: 1, scale: 1, ease: Back.easeOut.config(2.5), delay: 1 }, 0.2);
+    TweenMax.set(portfolioImage, {opacity: 0, scale: 0.6});
+    var staggerPortfolios = TweenMax.staggerTo(portfolioImage, 0.8, {opacity: 1, scale: 1, ease: Back.easeOut.config(2.5) }, 0.2);
+
+    var controller = new ScrollMagic.Controller();
+    var scene = new ScrollMagic.Scene({
+  		triggerElement: "#portfolio",	// the scene should last for a scroll distance of 100px
+  	}).setTween(staggerPortfolios).addTo(controller);
 
   }
   render(){
     return(
       <section className="portfolio">
         <center>
-          <h1>Portfolio</h1>
+        <div style={{height: 1000}} ></div>
+          <h1 id="portfolio">Portfolio</h1>
           <p>Six selected topics from different envirements I have worked on the past three years.</p>
           </center>
         <Loop portfolio={portfolios} />
+        <div style={{height: 1000}} ></div>
       </section>
     );
   }
